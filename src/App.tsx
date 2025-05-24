36acdbc3da338df7b2ed98bf06b6530fabcc528e
Arkamit <arkamitdutta@gmail.com>
2025-05-24 14:56:54 +0530

    Handling the click functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { useRef } from "react";
 import "./App.css";
 import BoxContainer from "./components/BoxContainer";
 
 function App() {
+  const selectedBoxes = useRef<string[]>([]);
+  
   return (
     <>
       {Array.from({ length: 3 }).map((_, i) => (
-        <BoxContainer id={`${i}`} key={i} />
+        <BoxContainer id={`${i}`} selectedBoxes={selectedBoxes} key={i} />
       ))}
     </>
   );
